d13ff6b3405c936d5b1ec279e2fe8f9a7e7d4c23
Calciu Raluca Cristina <calciuraluca23@stud.ase.ro>
2025-12-07 00:09:57 +0200

    Am actualizat fisierul pentru export

---
+++ server/router/routers.js
@@ -0,0 +1,6 @@
+const bugRouter = require("./bugRouter")
+const projectRouter = require("./projectRouter")
+const userRouter = require("./userRouter") 
+
+
+module.exports = {bugRouter, projectRouter, userRouter}
